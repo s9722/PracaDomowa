617e6088ea040d2c7ac08ff076973025b73c2eda
s9722 <s9722@pjwstk.edu.pl>
2018-11-06 14:07:16 +0100

    mae smiany

--- PracaDomowa/PracaDomowa/Program.cs
+++ PracaDomowa/PracaDomowa/Program.cs
@@ -12,21 +12,25 @@ namespace Praca_domowa
         {
             int podajCyfre1, podajCyfre2;
 
-            Console.Write("Podaj pierwsza cyfre: ");
+            Console.Write("Podaj pierwszą cyfrę: ");
             podajCyfre1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Podaj druga cyfre: ");
+            Console.Write("Podaj drugą cyfrę: ");
             podajCyfre2 = Convert.ToInt32(Console.ReadLine());
 
+            if (podajCyfre1 > 9) Console.WriteLine("To jest liczba a nie cyfra!");
+            if (podajCyfre2 > 9) Console.WriteLine("To jest liczba a nie cyfra!");
+            
             Console.WriteLine($"Iloczyn podanych cyfr to: {podajCyfre1 * podajCyfre2}");
 
             if (podajCyfre1 + podajCyfre2 > 15) Console.WriteLine($"Suma cyfr to: {podajCyfre1 + podajCyfre2}");
+
             else
             {
                 Console.WriteLine("Suma cyfr mniejsza od 15");
             }
 
             Console.ReadKey();
-
+                        
         }
     }
 }
